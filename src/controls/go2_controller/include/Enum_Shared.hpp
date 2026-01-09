c43dff5bcdf938920f0a456300b65ebf5179063f
taehyeok <phtoday213@seoultech.ac.kr>
2026-01-09 14:32:46 +0900

    TROT_GAIT 완성

--- src/controls/go2_controller/include/Enum_Shared.hpp
+++ src/controls/go2_controller/include/Enum_Shared.hpp
@@ -14,8 +14,8 @@ enum Walk_Parameter
     STANCE = 1,
     NUM_STATE = 2, 
     
-    T_SWING = 120,
-    T_STANCE = 380,
+    T_SWING = 150,
+    T_STANCE = 150,
     T_TROT = T_SWING + T_STANCE,
 };
 
